97fa36c8d5e1609ded93eb9be48fd51077ab4cb1
mbrrg <git@plushware.com>
2014-02-25 07:11:28 +0100

    Add STATIC_ROOT.

--- project/project/settings.py
+++ project/project/settings.py
@@ -108,5 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 WSGI_APPLICATION = 'project.wsgi.application'
